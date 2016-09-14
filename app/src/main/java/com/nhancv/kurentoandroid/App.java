634b5046118b8db1e4ca436d43c5c4368f28931e
Nhan Cao <nhancv1992@gmail.com>
2016-09-14 15:59:53 +0700

    initial

---
+++ app/src/main/java/com/nhancv/kurentoandroid/App.java
@@ -0,0 +1,15 @@
+package com.nhancv.kurentoandroid;
+
+import android.app.Application;
+
+/**
+ * Created by nhancao on 9/14/16.
+ */
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}

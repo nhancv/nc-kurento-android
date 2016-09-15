1393a4d7fd97f707c2b3f3a555f62b2fdc1c48aa
Nhan Cao <nhancv1992@gmail.com>
2016-09-15 09:52:28 +0700

    clean code prepare for webrtcpeer

--- app/src/main/java/com/nhancv/kurentoandroid/MainActivity.java
+++ app/src/main/java/com/nhancv/kurentoandroid/MainActivity.java
@@ -1,56 +1,17 @@
 package com.nhancv.kurentoandroid;
 
-import android.annotation.TargetApi;
-import android.net.http.SslError;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MotionEvent;
-import android.view.View;
-import android.webkit.CookieManager;
-import android.webkit.PermissionRequest;
-import android.webkit.SslErrorHandler;
-import android.webkit.WebChromeClient;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getName();
     String url = "https://192.168.1.59:8443";
-    WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        webView = (WebView) findViewById(R.id.webView);
-        setUpWebViewDefaults(webView);
-        webView.loadUrl(url);
-        webView.setWebChromeClient(new WebChromeClient() {
-            @Override
-            public void onPermissionRequest(final PermissionRequest request) {
-                runOnUiThread(new Runnable() {
-                    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-                    @Override
-                    public void run() {
-                        if (request.getOrigin().toString().contains(url)) {
-                            request.grant(request.getResources());
-                        } else {
-                            request.deny();
-                        }
-                    }
-                });
-            }
-        });
-    }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            webView.evaluateJavascript("if(window.localStream){window.localStream.stop();}", null);
-        }
     }
 
     @Override
@@ -59,53 +20,4 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    /**
-     * Convenience method to set some generic defaults for a given WebView
-     *
-     * @param webView
-     */
-    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-    private void setUpWebViewDefaults(WebView webView) {
-        WebSettings settings = webView.getSettings();
-        //Enable Javascript;
-        settings.setJavaScriptEnabled(true);
-        //Use WideViewport and Zoom out if there is no viewport defined
-        settings.setUseWideViewPort(true);
-        settings.setLoadWithOverviewMode(true);
-        //Enable pinch to zoom without the zoom buttons
-        settings.setBuiltInZoomControls(true);
-        //Allow use of Local Storage
-        settings.setDomStorageEnabled(true);
-        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.HONEYCOMB) {
-            //Hide the zoom controls for HONEYCOMB+
-            settings.setDisplayZoomControls(false);
-            //Load without cache
-            settings.setCacheMode(WebSettings.LOAD_NO_CACHE);
-        }
-        //Enable remote debugging via chrome://inspect
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            WebView.setWebContentsDebuggingEnabled(true);
-        }
-        webView.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                return false;
-            }
-        });
-        webView.setWebViewClient(new WebViewClient() {
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                view.loadUrl(url);
-                return false;
-            }
-
-            @Override
-            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
-                handler.proceed();
-            }
-        });
-        //AppRTC requires third party cookies to work
-        CookieManager cookieManager = CookieManager.getInstance();
-        cookieManager.setAcceptThirdPartyCookies(webView, true);
-    }
 }

2b40a8c972840c13bca4844eef0a186db3d91d46
Nhan Cao <nhancv1992@gmail.com>
2016-09-16 14:49:23 +0700

    implement call video but not sen video candidate yet - need sync with kurento flow server site: https://github.com/nhancv/ot-kurento-node-webrtc/tree/master/kurento-one2one-call

--- app/src/main/java/com/nhancv/kurentoandroid/MainActivity.java
+++ app/src/main/java/com/nhancv/kurentoandroid/MainActivity.java
@@ -1,5 +1,6 @@
 package com.nhancv.kurentoandroid;
 
+import android.graphics.Point;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -42,13 +43,31 @@ import fi.vtt.nubomedia.webrtcpeerandroid.NBMWebRTCPeer;
 
 public class MainActivity extends AppCompatActivity implements NBMWebRTCPeer.Observer {
     private static final String TAG = MainActivity.class.getName();
-    String host = "wss://192.168.1.59:7003/one2one";
+    // Local preview screen position before call is connected.
+    private static final int LOCAL_X_CONNECTING = 0;
+    private static final int LOCAL_Y_CONNECTING = 0;
+    private static final int LOCAL_WIDTH_CONNECTING = 100;
+    private static final int LOCAL_HEIGHT_CONNECTING = 100;
+    // Local preview screen position after call is connected.
+    private static final int LOCAL_X_CONNECTED = 72;
+    private static final int LOCAL_Y_CONNECTED = 72;
+    private static final int LOCAL_WIDTH_CONNECTED = 25;
+    private static final int LOCAL_HEIGHT_CONNECTED = 25;
+    // Remote video screen position
+    private static final int REMOTE_X = 0;
+    private static final int REMOTE_Y = 0;
+    private static final int REMOTE_WIDTH = 100;
+    private static final int REMOTE_HEIGHT = 100;
+    private static final String host = "wss://local.beesightsoft.com:7003/one2one";
     NBMWebRTCPeer nbmWebRTCPeer;
     NBMMediaConfiguration mediaConfiguration;
     LooperExecutor executor;
     WebSocketClient client;
+    String connectionId = "test1";
+    boolean coodinator;
+    private RendererCommon.ScalingType scalingType = RendererCommon.ScalingType.SCALE_ASPECT_FILL;
     private KeyStore keyStore;
-    private GLSurfaceView vsv;
+    private GLSurfaceView videoView;
     private VideoRenderer.Callbacks localRender;
     private VideoRenderer.Callbacks remoteRender;
 
@@ -66,7 +85,7 @@ public class MainActivity extends AppCompatActivity implements NBMWebRTCPeer.Obs
             public void onOpen(ServerHandshake serverHandshake) {
                 Log.i("Websocket", "Opened");
                 try {
-                    sendRegister();
+                    registerRequest();
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
@@ -74,35 +93,41 @@ public class MainActivity extends AppCompatActivity implements NBMWebRTCPeer.Obs
 
             @Override
             public void onMessage(String s) {
-                final String message = s;
-
+                Log.i(TAG, "onMessage: " + s);
                 try {
                     JSONObject obj = new JSONObject(s);
                     String command = obj.getString("id");
-                    if (command.equals("incomingCall")) {
+                    if (command.equals("registerResponse")) {
+//                        {"id":"registerResponse","response":"accepted"}
+                        registerResponse(obj.getString("response"));
+                    } else if (command.equals("callResponse")) {
+                        callResponse(obj.getString("response"), obj.getString("sdpAnswer"), connectionId);
+
+                    } else if (command.equals("incomingCall")) {
 //                        {"id":"incomingCall","from":"test1"}
                         String from = obj.getString("from");
-                        nbmWebRTCPeer.generateOffer(from, true);
+                        incomingCall(from);
 
+                    } else if (command.equals("startCommunication")) {
+                        startCommunication(obj.getString("sdpAnswer"), connectionId);
 
+                    } else if (command.equals("stopCommunication")) {
+                        stopCommunication();
 
+                    } else if (command.equals("iceCandidate")) {
 
-                    }else if (command.equals("registerResponse")) {
-//                        {"id":"registerResponse","response":"accepted"}
+                        JSONObject candidate = new JSONObject(obj.getString("candidate"));
+                        String sdpMid = candidate.getString("sdpMid");
+                        int sdpMLineIndex = candidate.getInt("sdpMLineIndex");
+                        String sdp = candidate.getString("candidate");
+
+                        iceCandidate(new IceCandidate(sdpMid, sdpMLineIndex, sdp));
 
                     }
 
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
-
-
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        Log.e(TAG, "run: " + message);
-                    }
-                });
             }
 
             @Override
@@ -171,47 +196,159 @@ public class MainActivity extends AppCompatActivity implements NBMWebRTCPeer.Obs
 
     private void initView() {
 
-        vsv = (GLSurfaceView) findViewById(R.id.glview_call);
-        vsv.setPreserveEGLContextOnPause(true);
-        vsv.setKeepScreenOn(true);
-        VideoRendererGui.setView(vsv, new Runnable() {
+        executor = new LooperExecutor();
+        executor.requestStart();
+
+        mediaConfiguration = new NBMMediaConfiguration();
+        videoView = (GLSurfaceView) findViewById(R.id.glview_call);
+        assert videoView != null;
+        videoView.setPreserveEGLContextOnPause(true);
+        videoView.setEGLContextClientVersion(2);
+        videoView.setKeepScreenOn(true);
+
+        VideoRendererGui.setView(videoView, new Runnable() {
             @Override
             public void run() {
-                executor = new LooperExecutor();
-                executor.requestStart();
-
-                localRender = VideoRendererGui.create(72, 72, 25, 25, RendererCommon.ScalingType.SCALE_ASPECT_FILL, false);
-                mediaConfiguration = new NBMMediaConfiguration();
-                nbmWebRTCPeer = new NBMWebRTCPeer(mediaConfiguration, MainActivity.this, localRender, MainActivity.this);
-                nbmWebRTCPeer.initialize();
-                nbmWebRTCPeer.enableVideo(true);
-                nbmWebRTCPeer.startLocalMedia();
-                connectWebSocket();
-
-
+                init();
             }
         });
+        // local and remote render
+        remoteRender = VideoRendererGui.create(
+                REMOTE_X, REMOTE_Y,
+                REMOTE_WIDTH, REMOTE_HEIGHT, scalingType, false);
+        localRender = VideoRendererGui.create(
+                LOCAL_X_CONNECTING, LOCAL_Y_CONNECTING,
+                LOCAL_WIDTH_CONNECTING, LOCAL_HEIGHT_CONNECTING, scalingType, true);
 
+    }
+
+    private void init() {
+        Point displaySize = new Point();
+        getWindowManager().getDefaultDisplay().getSize(displaySize);
+        connectWebSocket();
     }
 
     @Override
     public void onPause() {
         super.onPause();
-        vsv.onPause();
+        videoView.onPause();
     }
+
+    //Handle process
 
     @Override
     public void onResume() {
         super.onResume();
-        vsv.onResume();
+        videoView.onResume();
     }
 
-    public void sendRegister() throws JSONException {
+    public void registerRequest() throws JSONException {
         JSONObject obj = new JSONObject();
         obj.put("id", "register");
         obj.put("name", "test");
         send(obj);
     }
+
+    public void registerResponse(String response) {
+        if (response.equals("accepted")) {
+//            REGISTERED
+            call();
+        } else {
+//            NOT_REGISTERED
+        }
+    }
+
+    public void callResponse(String response, String sdpAnswer, String connectionId) {
+        Log.e(TAG, "callResponse: ");
+        if (!response.equals("accepted")) {
+            Log.e(TAG, "callResponse: Call not accepted by peer. Closing call");
+            stop(true);
+        } else {
+            nbmWebRTCPeer.processAnswer(new SessionDescription(SessionDescription.Type.ANSWER, sdpAnswer), connectionId);
+        }
+
+    }
+
+    public void incomingCall(String from) {
+        Log.e(TAG, "incomingCall: ");
+        coodinator = false;
+        connectionId = from;
+
+        try {
+            // If bussy just reject without disturbing user
+            // TODO: 9/15/16 handle != NO_CALL state
+//        if (callState != NO_CALL) {
+//            JSONObject obj = new JSONObject();
+//            obj.put("id", "incomingCallResponse");
+//            obj.put("from", connectionId);
+//            obj.put("callResponse", "reject");
+//            obj.put("message", "bussy");
+//            send(obj);
+//            return;
+//        }
+            // TODO: 9/15/16 Set PROCESSING_CALL
+            //Confirm accept true
+            nbmWebRTCPeer = new NBMWebRTCPeer(mediaConfiguration, MainActivity.this, localRender, MainActivity.this);
+            nbmWebRTCPeer.initialize();
+
+            nbmWebRTCPeer.generateOffer(connectionId, true);
+            //Confirm accept false
+//            JSONObject obj = new JSONObject();
+//            obj.put("id", "incomingCallResponse");
+//            obj.put("from", connectionId);
+//            obj.put("callResponse", "reject");
+//            obj.put("message", "user declined");
+//            send(obj);
+//            stop(true);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void startCommunication(String sdpAnswer, String connectionId) {
+        // TODO: 9/15/16 IN_CALL state
+        nbmWebRTCPeer.processAnswer(new SessionDescription(SessionDescription.Type.ANSWER, sdpAnswer), connectionId);
+    }
+
+    public void stopCommunication() {
+        stop(true);
+    }
+
+    public void iceCandidate(IceCandidate iceCandidate) {
+        nbmWebRTCPeer.addRemoteIceCandidate(iceCandidate, connectionId);
+    }
+
+    public void call() {
+        // TODO: 9/15/16 PROCESSING_CALL
+        Log.e(TAG, "call: ");
+
+        coodinator = true;
+        nbmWebRTCPeer = new NBMWebRTCPeer(mediaConfiguration, MainActivity.this, localRender, MainActivity.this);
+        nbmWebRTCPeer.initialize();
+        nbmWebRTCPeer.generateOffer(connectionId, true);
+    }
+
+    public void stop(boolean messageSent) {
+        // TODO: 9/15/16 NO_CALL state
+        Log.e(TAG, "stop: ");
+        if (nbmWebRTCPeer != null) {
+            if (!messageSent) {
+                try {
+                    JSONObject msg = new JSONObject();
+                    msg.put("id", "stop");
+                    send(msg);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+            client.close();
+            nbmWebRTCPeer.stopLocalMedia();
+            nbmWebRTCPeer.close();
+            nbmWebRTCPeer = null;
+        }
+    }
+
 
     public boolean isWebSocketConnected() {
         return client != null && client.getConnection().isOpen();
@@ -222,6 +359,7 @@ public class MainActivity extends AppCompatActivity implements NBMWebRTCPeer.Obs
             public void run() {
                 if (isWebSocketConnected()) {
                     try {
+                        Log.i(TAG, "send: " + message.toString());
                         client.send(message.toString().getBytes("UTF-8"));
                     } catch (UnsupportedEncodingException e) {
                         e.printStackTrace();
@@ -240,17 +378,63 @@ public class MainActivity extends AppCompatActivity implements NBMWebRTCPeer.Obs
 
     @Override
     public void onLocalSdpOfferGenerated(SessionDescription localSdpOffer, NBMPeerConnection connection) {
-        Log.e(TAG, "onLocalSdpOfferGenerated: " + localSdpOffer);
+        Log.e(TAG, "onLocalSdpOfferGenerated: " + localSdpOffer.type + " -" + localSdpOffer.description);
+        if (coodinator) {
+            try {
+                JSONObject obj = new JSONObject();
+                obj.put("id", "call");
+                obj.put("from", "test");
+                obj.put("to", connectionId);
+                obj.put("sdpOffer", localSdpOffer.description);
+                send(obj);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        } else {
+            try {
+                JSONObject obj = new JSONObject();
+                obj.put("id", "incomingCallResponse");
+                obj.put("from", connectionId);
+                obj.put("callResponse", "accept");
+                obj.put("sdpOffer", localSdpOffer.description);
+                send(obj);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
     }
 
     @Override
     public void onLocalSdpAnswerGenerated(SessionDescription localSdpAnswer, NBMPeerConnection connection) {
-        Log.e(TAG, "onLocalSdpAnswerGenerated: " + localSdpAnswer);
+//        Log.e(TAG, "onLocalSdpAnswerGenerated: " + localSdpAnswer.type + " -" + localSdpAnswer.description);
+//        try {
+//            JSONObject obj = new JSONObject();
+//            obj.put("id", "incomingCallResponse");
+//            obj.put("from", connectionId);
+//            obj.put("callResponse", "accept");
+//            obj.put("sdpOffer", localSdpAnswer.description);
+//            send(obj);
+//        } catch (JSONException e) {
+//            e.printStackTrace();
+//        }
     }
 
     @Override
     public void onIceCandidate(IceCandidate localIceCandidate, NBMPeerConnection connection) {
         Log.e(TAG, "onIceCandidate: " + localIceCandidate);
+        try {
+            JSONObject obj = new JSONObject();
+            obj.put("id", "onIceCandidate");
+            JSONObject candidate = new JSONObject();
+            candidate.put("candidate", localIceCandidate.sdp);
+            candidate.put("sdpMid", localIceCandidate.sdpMid);
+            candidate.put("sdpMLineIndex", localIceCandidate.sdpMLineIndex);
+            obj.put("candidate", candidate);
+            send(obj);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -262,11 +446,23 @@ public class MainActivity extends AppCompatActivity implements NBMWebRTCPeer.Obs
     public void onRemoteStreamAdded(MediaStream stream, NBMPeerConnection connection) {
         Log.e(TAG, "onRemoteStreamAdded: ");
         nbmWebRTCPeer.attachRendererToRemoteStream(remoteRender, stream);
+        VideoRendererGui.update(remoteRender,
+                REMOTE_X, REMOTE_Y,
+                REMOTE_WIDTH, REMOTE_HEIGHT, scalingType, false);
+        VideoRendererGui.update(localRender,
+                LOCAL_X_CONNECTED, LOCAL_Y_CONNECTED,
+                LOCAL_WIDTH_CONNECTED, LOCAL_HEIGHT_CONNECTED,
+                scalingType, true);
     }
 
     @Override
     public void onRemoteStreamRemoved(MediaStream stream, NBMPeerConnection connection) {
         Log.e(TAG, "onRemoteStreamRemoved: ");
+        VideoRendererGui.update(localRender,
+                LOCAL_X_CONNECTING, LOCAL_Y_CONNECTING,
+                LOCAL_WIDTH_CONNECTING, LOCAL_HEIGHT_CONNECTING,
+                scalingType, true);
+
     }
 
     @Override
